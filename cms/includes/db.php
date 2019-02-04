1dd14fbe051c98ce30aef9045ef3206b1a7b8d78
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2019-02-04 12:34:25 -0600

    Started project and hooked up database

---
+++ cms/includes/db.php
@@ -0,0 +1,18 @@
+<?php
+
+//opens connection to database
+//server name, username, password, db name
+
+$db_user = 'localhost';
+$password = '';
+$root = 'root';
+$databaseName = 'cms';
+
+
+$connection = mysqli_connect($db_user,$root,'',$databaseName);
+
+
+
+
+
+?>
---
+++ cms/index.php
@@ -0,0 +1,41 @@
+
+<?php  
+include "includes/db.php";
+include "includes/header.php";
+
+  ?>
+<body>
+
+
+    <!-- Navigation -->
+    <?php  
+
+include "includes/nav.php";
+
+  ?>
+
+    <!-- Page Content -->
+   <?php include "includes/content.php"; ?>
+
+                <!-- Pager -->
+                <ul class="pager">
+                    <li class="previous">
+                        <a href="#">&larr; Older</a>
+                    </li>
+                    <li class="next">
+                        <a href="#">Newer &rarr;</a>
+                    </li>
+                </ul>
+
+            </div>
+
+            
+        <hr>
+  <?php   include "includes/sideBar.php"; ?>
+        <!-- Footer -->
+        <?php 
+    include "includes/footer.php";
+
+        ?>
+
+       
