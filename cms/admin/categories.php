a7bbee08c152dbbc651fe4789c179f90a8990f9c
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2019-02-05 12:08:31 -0600

    Added form to database

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -19,7 +19,33 @@ Welcomee
 </h1>
 
 <div class="col-xs-6">
-<form action="">
+<?php 
+
+if(isset($_POST['submit'])) {
+
+	$cat_title = $_POST['cat_title'];
+
+	 if($cat_title == "" || empty($cat_title)){
+	 	echo "Input can not be blank.";
+	 } else {
+
+	 	$query = "INSERT INTO categories(cat_title) ";
+	 	$query .= "VALUE('{$cat_title}') ";
+	 	$create_category_query = mysqli_query($connection, $query);
+
+	 	if(!$create_category_query){
+
+	 		die('Something went wrong' > mysqli_error($connection));
+	 	}
+	 }
+
+
+}
+
+
+?>
+
+<form action="" method="post">
 <div class="form-group">
 	<label for="cat_title">Category Title</label>
 	<input class="form_control" type="text" name="cat_title">
